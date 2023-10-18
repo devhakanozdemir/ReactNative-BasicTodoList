26447aa54b9b4b93c5b1e361f2a32cc6237b9d06
devhakanozdemir <dev.hakanozdemir@gmail.com>
2023-10-18 23:29:19 +0300

    create main page, useStates and save pressables

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+} from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+
+  const [value, setValue] = useState("")
+  const [value2, setValue2] = useState([])
+
+  const handleButton = () => {
+    setValue2([...value2, value])
+    setValue("")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder='You can add todos here'
+        onChangeText={setValue}
+        value={value}
+      />
+
+      <View style={styles.container2}>
+
+        {
+          value2.map((item, index) => {
+            return (
+              <View key={index} style={styles.listStyle}>
+                <Text>{item}</Text>
+              </View>
+            )
+          })
+        }
+
+      </View>
+
+      <Pressable
+        style={styles.buttonStyle}
+        onPress={() => handleButton()}
+      >
+        <Text>Kaydet</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,4 +58,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  inputStyle: {
+    backgroundColor: "tomato",
+    width: "80%",
+    height: 50,
+    borderRadius: 15,
+    textAlign: "center",
+  },
+
+  buttonStyle: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "green",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+
+  container2: {
+    width: "80%",
+    height: 400,
+    backgroundColor: "yellow",
+    marginTop: 15,
+    alignItems: "center",
+    padding: 20,
+  },
+
+  listStyle: {
+    width: 200,
+    height: 30,
+    backgroundColor: "green",
+    marginBottom: 5,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  }
 });
